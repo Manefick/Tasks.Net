fd32e12bccb3f83989d0a046ec8224b607f4b0d1
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-19 14:00:42 +0300

    Задача 4 (Делегаты). Версия 1.0

---
+++ Task4_Delegate/DelegateFunc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_Delegate
+{
+    class DelegateFunc
+    {
+        public static void SortArray(int [] array, Sort sort)
+        {
+            int temp;
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                for(int j = i+1; j < array.Length; j++)
+                {
+                    if(sort(array[i],array[j]))
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            foreach (int k in array)
+                Console.Write(k + " ");
+        }
+        public delegate bool Sort(int x, int y);
+
+        public static bool Asc(int x, int y)
+        {
+            if (x > y)
+                return true;
+            else
+                return false;
+        }
+        public static bool Desc(int x, int y)
+        {
+            if (x < y)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- Task4_Delegate/Program.cs
+++ Task4_Delegate/Program.cs
@@ -10,7 +10,27 @@ namespace Task4_Delegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( );
+            //Console.WriteLine("Введите массив целых чисел через запятую");
+            //int[] myArray = Console.ReadLine().Split(',').Select(n=>int.Parse(n)).ToArray<int>();
+            Random n = new Random();
+            int[] myArray = new int[10];
+            for(int i = 0; i<10;i++)
+            {
+                myArray[i] = n.Next(1, 100);
+            }
+
+            Console.WriteLine("Введите цифру 1 если хотите отсортировать масив по возростанию, цифру 2 -по убыванию");
+            int choise = int.Parse(Console.ReadLine());
+            if(choise ==1)
+                DelegateFunc.SortArray(myArray, new DelegateFunc.Sort(DelegateFunc.Asc));
+            else if(choise==2)
+                DelegateFunc.SortArray(myArray, new DelegateFunc.Sort(DelegateFunc.Desc));
+            else
+                Console.WriteLine("Вы нажали не правильную цыфруб попробуйте еще раз");
+
+
+            Console.ReadLine();
         }
+        
     }
 }
