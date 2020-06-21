e1c4d3bec4ab18e49be0b49633c0db8ff3fefbf7
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-21 11:20:54 +0300

    Task Shape version 1.0

--- Task3_Shape/Program.cs
+++ Task3_Shape/Program.cs
@@ -10,7 +10,44 @@ namespace Task3_Shape
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("fdfdfd"); 
+            Console.WriteLine("Нажмите цифру которая соотвествует названию фигуры площадь которой хотите найти\n" +
+                "1-Square\n2-Circle\n3-Treangle");
+            int choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("print side of square");
+                    double SideSquare = double.Parse(Console.ReadLine());
+                    Console.WriteLine("print name of shape ");
+                    string NameShape = Console.ReadLine();
+                    Square square = new Square(NameShape, SideSquare);
+                    UserInterface.ShapeArea(square);
+                    break;
+                case 2:
+                    Console.WriteLine("print radius of circle");
+                    double RadiusCircle = double.Parse(Console.ReadLine());
+                    Console.WriteLine("print name of shape ");
+                    string CircleName = Console.ReadLine();
+                    Circle circle = new Circle(CircleName, RadiusCircle);
+                    UserInterface.ShapeArea(circle);
+                    break;
+                case 3:
+                    Console.WriteLine("print side1 of Treangle");
+                    double TreangleSide1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("print side2 of Treangle");
+                    double TreangleSide2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("print side3 of Treangle");
+                    double TreangleSide3 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("print name of shape ");
+                    string TreangleName = Console.ReadLine();
+                    Triangle triangle = new Triangle(TreangleName, TreangleSide1, TreangleSide2, TreangleSide3);
+                    UserInterface.ShapeArea(triangle);
+                    break;
+                default:
+                    Console.WriteLine("You enter the wrong number please try again!!!");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Task3_Shape/Shape.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_Shape
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; } = "Shape";
+
+        public Shape(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name do not be null");
+            }
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"Площадь {Name} = {Area()}";
+        }
+        public virtual double Area()
+        {
+            return 0.0;
+        }
+    }
+
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public Square(string name, double side) : base(name)
+        {
+            Side = side;
+        }
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.Pow(Radius, 2) * Math.PI;
+        }
+    }
+    public class Triangle : Shape
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+        private double Perimeter { get { return (Side1 + Side2 + Side3) / 2; } }
+
+        public Triangle(string name, double side1, double side2, double side3) : base(name)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+        public override double Area()
+        {
+            return Math.Sqrt(Perimeter * (Perimeter - Side1) * (Perimeter - Side2) * (Perimeter - Side3));
+        }
+    }
+}
