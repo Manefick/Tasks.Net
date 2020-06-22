ed4077c63ccebd02dc8b88609e29c5e1cba9f40c
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-22 14:15:02 +0300

    task1 airoport  добавил большую часть функционала

--- Task1_Airoport/AiroportTable.cs
+++ Task1_Airoport/AiroportTable.cs
@@ -10,6 +10,20 @@ namespace Task1_Airoport
     {
         private static List<object[]> table = new List<object[]>();
 
+        public static void testInfo()
+        {
+            object[] vs1 = { 690, "qqq", "www", 20, "B", "1F", new DateTime(2020, 9, 20, 22, 22, 00), "unknown" };
+            object[] vs2 = { 305, "eee;", "rrr", 7, "P", "5F", new DateTime(2020, 10, 21, 22, 12, 00), "unknown" };
+            object[] vs3 = { 33, "ttt", "yyy", 55, "E", "2F", new DateTime(2020, 6, 5, 22, 5, 00), "unknown" };
+            object[] vs4 = { 404, "uuu", "iii", 6, "G", "11C", new DateTime(2020, 9, 20, 22, 22, 00), "unknown" };
+            object[] vs5 = { 506, "ooo", "ppp", 8, "L", "4D", new DateTime(2020, 9, 20, 22, 22, 00), "unknown" };
+            table.Add(vs1);
+            table.Add(vs2);
+            table.Add(vs3);
+            table.Add(vs4);
+            table.Add(vs5);
+        }
+
         public static void AddFly()
         {
             Console.WriteLine("Print the fly number, and press 'ENTER' ");
@@ -33,6 +47,55 @@ namespace Task1_Airoport
 
             object[] flyInfo = {flyNumber,cityDeparture,cityArrival,airlineNumber,terminal,gate,dateTime,flightStatus};
             table.Add(flyInfo);
+            ShowTableInfo();
+
+        }
+        public static void DeleteFly()
+        {
+            Console.WriteLine("Enter the fly number that you wont delete");
+            int flyNumDel = int.Parse(Console.ReadLine());
+            for(int i=0;i<table.Count();i++)
+            {
+                if ((int)table[i][0] == flyNumDel)
+                    table.RemoveAt(i);
+            }
+            ShowTableInfo();
+        }
+        public static void EditFly()
+        {
+            Console.WriteLine("Enter the fly number that you wont Edit");
+            int flyNumEdit = int.Parse(Console.ReadLine());
+            Console.WriteLine("What info you wont change? If you wont change cityDeparture--press 1,\n" +
+                "cityArrival--press 2,airlineNumber--press 3,terminal--press 4,gate--press 5,\n" +
+                "dateTime--press 6,flightStatus--press 7, end press 'ENTER' ");
+            int choise = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write new info ");
+            string newInfo = Console.ReadLine();
+            int index = 0;
+            for (int i = 0; i < table.Count(); i++)
+            {
+                if ((int)table[i][0] == flyNumEdit)
+                    break;
+                index++;
+            }
+            switch (choise)
+            {
+                case 1:
+                    table[index][1] = newInfo;
+                    Console.WriteLine(index);
+                    break;
+                case 2:
+                    table[index][2] = newInfo;
+                    Console.WriteLine(index);
+                    break;
+            }
+            ShowTableInfo();
+
+        }
+        public static void ShowTableInfo()
+        {
+            foreach(var i in table)
+                Console.WriteLine($"{i[0]} {i[1]} {i[2]} {i[3]} {i[4]} {i[5]} {i[6]} {i[7]}");
         }
     }
 }
--- Task1_Airoport/Program.cs
+++ Task1_Airoport/Program.cs
@@ -10,8 +10,29 @@ namespace Task1_Airoport
     {
         static void Main(string[] args)
         {
-            
-
+            AiroportTable.testInfo();
+            AiroportTable.ShowTableInfo();
+            Console.WriteLine("If you wont input fly info press '1'\nIf you wont delete fly info press '2'\n" +
+                "If you wont etit fly info press '3'\nShow fly table press '4', end press 'ENTER'");
+            int choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    AiroportTable.AddFly();
+                    break;
+                case 2:
+                    AiroportTable.DeleteFly();
+                    break;
+                case 3:
+                    AiroportTable.EditFly();
+                    break;
+                case 4:
+                    AiroportTable.ShowTableInfo();
+                    break;
+                default:
+                    AiroportTable.ShowTableInfo();
+                    break;
+            }
             Console.ReadLine();
         }
     }
