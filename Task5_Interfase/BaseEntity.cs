7837d76730252981f08a0485528ff503f797be23
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-22 11:15:55 +0300

    Пятое задание Interfase, сделано 70%

---
+++ Task5_Interfase/BaseEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_Interfase
+{
+    abstract class BaseEntity
+    {
+        internal int Id { get; set; }
+        internal string Name { get; set; }
+        internal int Price { get; set; }
+
+        public BaseEntity() { }
+        public BaseEntity(int id, string name, int price)
+        {
+            Id = id;
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Поле не может быть пустым");
+            Name = name;
+            if (price < 0)
+                throw new ArgumentException("Цена не может быть меньше 0");
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Id:{Id}, Name: {Name}, Price:{Price}";
+        }
+    }
+
+
+}
---
+++ Task5_Interfase/Storage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_Interfase
+{
+    class Storage:IEntityService<BaseEntity>
+    {
+        private List<BaseEntity> baseEntities = new List<BaseEntity>();
+        public void Add(BaseEntity ob)
+        {
+            baseEntities.Add(ob);
+        } 
+        public void Delete(BaseEntity en)
+        {
+            baseEntities.Remove(en);
+        }
+        public BaseEntity Find(int id)
+        {
+            return baseEntities.Find(delegate (BaseEntity entity)
+            { return entity.Id == id; });
+        }
+        public BaseEntity[] GetAll()
+        {
+            BaseEntity[] bases = baseEntities.ToArray();
+            return bases;
+        }
+    }
+
+    public interface IEntityService<T>
+    {
+        T Find(int id);
+        void Add(T ob);
+        void Delete(T en);
+        T[] GetAll();
+    } 
+}
