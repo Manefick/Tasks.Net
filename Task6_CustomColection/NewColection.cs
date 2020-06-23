6e30a2ec8b29ea1e5aed492ca1083ee9fdd4e048
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-23 11:38:29 +0300

    Task 6 CustomColection --Done Не знаю как по другому перегрузить итератор и как передать конструктору который принимает два значения одно

---
+++ Task6_CustomColection/NewColection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_CustomColection
+{
+    class NewColection: IEnumerable
+    {
+        public int division { get; } = 1; 
+        public int rangeMin { get; } 
+        public int rangeMax { get; }
+
+        private int[] CustomColection = new int[100];
+
+        public NewColection(int div)
+        {
+            InitializationNewColection();
+            division = div;
+        }
+        public NewColection(int rMin=0, int rMax=100)
+        {
+            InitializationNewColection();
+            rangeMin = rMin;
+            rangeMax = rMax;
+        }
+        private void InitializationNewColection()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+                CustomColection[i] = random.Next(-100, 100);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            foreach (int x in CustomColection)
+            {
+                if ((x % division == 0) && (rangeMin < x) && (x < rangeMax))
+                    yield return x;
+            }
+        }
+
+    }
+}
---
+++ Task6_CustomColection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_CustomColection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewColection newColection = new NewColection(9,80);// как сделать по условию?
+            foreach (var i in newColection)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
+
+        }
+    }
+}
