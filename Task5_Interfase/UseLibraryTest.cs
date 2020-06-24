ebac13e12b0505133054573549add14342ca3357
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-06-24 13:52:29 +0300

    использовал свою сборку по ссилке

---
+++ Task5_Interfase/UseLibraryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLibrary;
+
+namespace Task5_Interfase
+{
+    class UseLibraryTest:Car
+    {
+        private int RunCar { get; } = 0;
+        public UseLibraryTest(string b, string m, int y, bool n, int rc) : base(b, m, y,n)
+        {
+            if (CarStateNew==false)
+                RunCar = rc;
+        }
+    }
+}
